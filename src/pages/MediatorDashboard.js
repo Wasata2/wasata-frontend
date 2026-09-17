83fb5f07e0e8b6094a0fa165b7fdb538a234ebea
basmala aburass <basmalaaburass67@gmail.com>
2026-09-17 13:28:24 +0300

    new update

--- src/pages/MediatorDashboard.js
+++ src/pages/MediatorDashboard.js
@@ -87,12 +87,6 @@ export default function MediatorDashboard() {
           <Link to="/mediator-services" className="sidebar-link">
             <span className="sidebar-icon">🛍</span> الخدمات
           </Link>
-          <a href="#" className="sidebar-link">
-            <span className="sidebar-icon">📅</span> الجدول والسعة
-          </a>
-          <a href="#" className="sidebar-link">
-            <span className="sidebar-icon">💳</span> المالية
-          </a>
           <Link to="/mediator-reviews" className="sidebar-link">
             <span className="sidebar-icon">⭐</span> التقييمات
           </Link>
@@ -194,7 +188,9 @@ export default function MediatorDashboard() {
             <Link to="/mediator-services" className="btn btn-primary">
               + إضافة خدمة
             </Link>
-            <button className="btn btn-outline">تعديل الجدول والسعة</button>
+            <Link to="/mediator-reviews" className="btn btn-primary">
+            عرض التقييمات 
+            </Link>
             <Link to="/mediator-orders" className="btn btn-outline">
               عرض الطلبات
             </Link>
