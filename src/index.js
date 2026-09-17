098cfd53aadb50c1d3238cea149c642317587ade
Aya <ayanaeem900@gmail.com>
2026-09-17 13:56:45 +0300

    login update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { AuthProvider } from './context/AuthContext';   {/* ← سطر جديد */}
+import { AuthProvider } from './context/AuthContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>        {/* ← سطر جديد */}
+      <AuthProvider>
         <App />
-      </AuthProvider>       {/* ← سطر جديد */}
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
